f8deddfa12459d3d1cf787e94f6e84387e2596f7
pedro <pedro@Not-Kub>
2016-02-14 18:07:40 -0200

    Ainda incompleto - não funcional

--- src/algoritmogenetico1/Populacao.java
+++ src/algoritmogenetico1/Populacao.java
@@ -45,25 +45,53 @@ public class Populacao {
     //verifica se algum indivíduo da população possui a solução
     public boolean temSolucao(String solucao){
         Individuo i = null;
-        for(int j=0;j<individuos.length;j++){
-            if (individuos[j].getGenes().equals(solucao)){
-                i = individuos[j];
+        for (Individuo individuo : individuos) {
+            if (individuo.getGenes().equals(solucao)) {
+                i = individuo;
                 break;
             }
         }
-        if (i==null){
-            return false;
-        }
-        return true;
+        return i != null;
         
     }
     
+    //ordena a população pelo valor de aptidao de cada individuo, do maior valor
+    //para o menor, assim se eu quiser obter o melhor individuo desta população
+    //acesso a posição 0 do array de individuos.
+    
+    public void ordenaPopulacao(){
+        boolean trocou = true;
+        while(trocou){
+            trocou = false;
+            for(int i = 0; i<individuos.length-1;i++){
+                if(individuos[i].getAptidao() < individuos[i+1].getAptidao()){
+                    Individuo temp = individuos[i];
+                    individuos[i] = individuos[i+1];
+                    individuos[i+1] = temp;
+                    trocou = true;
+                }
+            }
+        } 
+    }
     
     
-    
-    
-    
-    
-    
+    //número de invíduos existentes na população
+    public int getNumIndividuos(){
+        int num = 0;
+        for(int i = 0; i < individuos.length;i++){
+            if (individuos[i] != null){
+                num++;
+            }
+        }
+        return num;
+    }
+
+    public Individuo[] getIndividuos() {
+        return individuos;
+    }
+
+    public int getTamPopulacao() {
+        return tamPopulacao;
+    }
     
 }
