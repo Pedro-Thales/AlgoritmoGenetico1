e17ecae8b3ebf5f6941aa75e991cc0527649704e
pedro <pedro@Not-Kub>
2016-02-15 22:16:14 -0200

    Versão 1.0 - Bug em palavras com menos de 6 caracteres

--- src/algoritmogenetico1/AlgoritmoGenetico1.java
+++ src/algoritmogenetico1/AlgoritmoGenetico1.java
@@ -16,13 +16,58 @@ public class AlgoritmoGenetico1 {
      */
     public static void main(String[] args) {
         //definir a solução
-        Algoritimo.setSolucao("Paralelepípedo");
+        Algoritmo.setSolucao("Nayara");
         //definir os caracteres existentes
-        Algoritimo.setCaracteres("qwertyuiopasdfghjklçzxcvbnmíéaáàúóãõ QWERTYUIOPASDFGHJKLÇZXCVBNMÍÉÁÀÓÃÕ");
-        
-        
-        
-        
+        Algoritmo.setCaracteres("qwertyuiopasdfghjklçzxcvbnmíéaáàúóãõ QWERTYUIOPASDFGHJKLÇZXCVBNMÍÉÁÀÓÃÕ");
+        //taxa de crossover de 60%
+        Algoritmo.setTaxaDeCrossover(0.6);
+        //mutacao de 3%
+        Algoritmo.setTaxaDeMutacao(0.3);
+        //elitismo
+        boolean elitismo = true;
+        //tamanho da populaçao
+        int tamPop = 100;
+        //numero maximo de geraçoes
+        int numMaxGeracoes = 10000;
+        //define o numero de genes do individuo baseado na soluçao
+        int numGenes = Algoritmo.getSolucao().length();
+        //cria a primeira populacao aleatoria 
+        Populacao populacao = new Populacao(numGenes, tamPop);
+
+        boolean temSolucao = false;
+        int geracao = 0;
+
+        System.out.println("Iniciando... Aptidao da soluçao:"
+                + Algoritmo.getSolucao().length());
+
+        //loop ate o criterio de parada
+        while (!temSolucao && geracao < numMaxGeracoes) {
+            geracao++;
+
+            //cria nova populacao 
+            populacao = Algoritmo.novaGeracao(populacao, elitismo);
+
+            System.out.println("Geração " + geracao + " | Aptidão: "
+                    + populacao.getIndividuo(0).getAptidao()
+                    + " | Melhor: " + populacao.getIndividuo(0).getGenes());
+
+            //verifica se tem a solucao
+            temSolucao = populacao.temSolucao(Algoritmo.getSolucao());
+
+        }
+
+        if (geracao == numMaxGeracoes) {
+            System.out.println("Número máximo de gerações | "
+                    + populacao.getIndividuo(0).getGenes() + " "
+                    + populacao.getIndividuo(0).getAptidao());
+        }
+
+        if (temSolucao) {
+            System.out.println("Encontrado resultado na geração " + geracao
+                    + " | " + populacao.getIndividuo(0).getGenes()
+                    + " (Aptidão: " + populacao.getIndividuo(0).getAptidao() + ")");
+        }
+
     }
-    
+
 }
